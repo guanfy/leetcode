c0626f0631643b6da426a1183757cc8abdbfd2b6
guanfy <30295319+guanfy@users.noreply.github.com>
2017-09-19 16:58:49 +0800

    Update fenghuo02.cpp

--- fenghuo02.cpp
+++ fenghuo02.cpp
@@ -3,14 +3,19 @@
 using namespace std;
 
 char arr[100][100];
+char res[100][100];
 int main()
 {
 	int n, m;
+	int r_i = 0, r_j = 0;
 	int left = 0, right = 0;
+	int i_start = 0, i_end = 0;
 	while (cin >> n && cin >> m)
 	{
 		left = m - 1;
 		right = 0;
+		i_start = n - 1;
+		i_end = 0;
 		for (int i = 0; i < n; i++)
 		{
 			for (int j = 0; j < m; j++)
@@ -18,7 +23,7 @@ int main()
 				cin >> arr[i][j];
 			}
 		}
-		
+
 		for (int i = 0; i < n; i++)
 		{
 			int j = 0, k = m - 1;
@@ -34,7 +39,26 @@ int main()
 			}
 			left = min(left, j);
 			right = max(right, k);
+			i_start = min(i_start, i);
+			i_end = max(i_end, i);
+		}
+		
+		for (int i = i_start; i <= i_end; i++)
+		{
+			r_j = 0;
+			for (int j = left; j <= right; j++)
+			{
+				res[r_i][r_j++] = arr[i][j];
+			}
+			r_i++;
 		}
 	}
+	for (int i = 0; i < r_i; i++)
+	{
+		for (int j = 0; res[i][j] != '\0'; j++)
+			cout << res[i][j];
+		cout << endl;
+	}
+
 	return 0;
 }
